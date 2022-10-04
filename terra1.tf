7723ab8505f929192e3626ea4ee9cc01e013ce2f
kyrier <data.mail.wild@gmail.com>
2022-10-04 20:43:42 +0200

    add lifecycle

--- terra1.tf
+++ terra1.tf
@@ -6,6 +6,8 @@ variable "vpc_security_group_ids"  {
     "default" = ["sg-0bb878c4011e7c539"]
   }
 }
+
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
@@ -13,8 +15,9 @@ provider "aws" {
 }
 
 
+
 resource "aws_instance" "my_ubuntu" {
-      count                  = 3
+      count                  = 1
       ami                    = "ami-08c40ec9ead489470"
       instance_type          = "t3.small"
       vpc_security_group_ids = var.vpc_security_group_ids["default"]
@@ -24,13 +27,18 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "The page was created by the user data" | sudo tee /var/www/html/index.html
+echo "The page was created by the user data." | sudo tee /var/www/html/index.html
 EOF
 
       tags = {
         Name                 = "aws"
         Owner                = "Kyrylo Iershov"
         Project              = "Terraform practice"
+      }
+
+
+      lifecycle {
+        create_before_destroy = true
       }
     }
     
